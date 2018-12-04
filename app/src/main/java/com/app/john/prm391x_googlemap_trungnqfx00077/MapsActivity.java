99204096b720427675882d957d3a43dba4d28a48
TrungNguyen <trung.nguyen@triphunter.vn>
2018-12-04 18:00:00 +0700

    add place autocomplete

--- app/src/main/java/com/app/john/prm391x_googlemap_trungnqfx00077/MapsActivity.java
+++ app/src/main/java/com/app/john/prm391x_googlemap_trungnqfx00077/MapsActivity.java
@@ -2,7 +2,14 @@ package com.app.john.prm391x_googlemap_trungnqfx00077;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,6 +21,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    SupportPlaceAutocompleteFragment mInputFrom;
+    SupportPlaceAutocompleteFragment mInputTo;
+
+    Place mFromPlace;
+    Place mToPlace;
+
+    Button mDirectionBtn;
+    TextView mDistanceTV;
+    TextView mTimeRouteTV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,7 +38,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        mDirectionBtn = findViewById(R.id.mDirectionBtn);
+        mDistanceTV = findViewById(R.id.mDistanceTV);
+        mTimeRouteTV = findViewById(R.id.mTimeRouteTV);
+
+        mInputFrom = (SupportPlaceAutocompleteFragment) getSupportFragmentManager().findFragmentById(R.id.place_autocomplete_fragment_from);
+        if (mInputFrom != null) {
+            mInputFrom.setHint("From");
+            mInputFrom.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                @Override
+                public void onPlaceSelected(Place place) {
+                    mFromPlace = place;
+                }
+
+                @Override
+                public void onError(Status status) {
+                    Log.d("mInputFrom: ", status.getStatusMessage());
+                }
+            });
+        }
+
+        mInputTo = (SupportPlaceAutocompleteFragment) getSupportFragmentManager().findFragmentById(R.id.place_autocomplete_fragment_to);
+        if (mInputTo != null) {
+            mInputTo.setHint("To");
+            mInputTo.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                @Override
+                public void onPlaceSelected(Place place) {
+                    mToPlace = place;
+                }
+
+                @Override
+                public void onError(Status status) {
+                    Log.d("mInputTo: ", status.getStatusMessage());
+                }
+            });
+        }
     }
 
 
@@ -40,7 +95,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        );
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
