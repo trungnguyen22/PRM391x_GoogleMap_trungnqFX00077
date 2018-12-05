fbb7ea29a713e9559c1de8f3865438e43db71fff
TrungNguyen <trung.nguyen@triphunter.vn>
2018-12-05 17:54:07 +0700

    permission

---
+++ app/src/main/java/com/app/john/prm391x_googlemap_trungnqfx00077/Constants.java
@@ -0,0 +1,11 @@
+package com.app.john.prm391x_googlemap_trungnqfx00077;
+
+public class Constants {
+
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 22;
+
+    // Dinh Doc Lap
+    public static final double DEFAULT_LATITUDE = 10.777171;
+    public static final double DEFAULT_LONGITUDE = 106.695503;
+
+}
--- app/src/main/java/com/app/john/prm391x_googlemap_trungnqfx00077/MapsActivity.java
+++ app/src/main/java/com/app/john/prm391x_googlemap_trungnqfx00077/MapsActivity.java
@@ -1,23 +1,40 @@
 package com.app.john.prm391x_googlemap_trungnqfx00077;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import static com.app.john.prm391x_googlemap_trungnqfx00077.Constants.MY_PERMISSIONS_REQUEST_LOCATION;
+
+public class MapsActivity extends FragmentActivity
+        implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
 
@@ -31,18 +48,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     TextView mDistanceTV;
     TextView mTimeRouteTV;
 
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location mMyLocation;
+
+    private boolean mPermissionDenied;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        initMap();
+        initViews();
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        requestLocationPermission();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    private void initMap() {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+    }
 
-        mDirectionBtn = findViewById(R.id.mDirectionBtn);
+    private void initViews() {
+        findViewById(R.id.mDirectionBtn).setOnClickListener(this);
+        findViewById(R.id.mMyLocationBtn).setOnClickListener(this);
         mDistanceTV = findViewById(R.id.mDistanceTV);
         mTimeRouteTV = findViewById(R.id.mTimeRouteTV);
 
@@ -79,6 +122,86 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    private void requestLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle(R.string.title_location_permission)
+                        .setMessage(R.string.text_location_permission)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MapsActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION
+                );
+            }
+        } else {
+            getLastLocation();
+        }
+
+    }
+
+    @SuppressLint("all")
+    private void getLastLocation() {
+        if (mMap != null) mMap.setMyLocationEnabled(true);
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            mMyLocation = location;
+                            focusOnMyLocation(location);
+                        }
+                    }
+                });
+    }
+
+    private void focusOnMyLocation(Location location) {
+        LatLng lastLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLatLng, 15f));
+        mMap.addMarker(new MarkerOptions()
+                .title("Your location")
+                .position(lastLatLng));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case Constants.MY_PERMISSIONS_REQUEST_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        getLastLocation();
+                    }
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    mPermissionDenied = true;
+                }
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -89,17 +212,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney")
-        );
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mDirectionBtn:
+                // TODO: Call API get direction
+                break;
+            case R.id.mMyLocationBtn:
+                focusOnMyLocation(mMyLocation);
+                break;
+        }
+    }
+
 }
